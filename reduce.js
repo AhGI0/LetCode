b2b17daf5ce25e5cff23fe6ddf5abafe4ea79068
Ahemd <ahmedsaye23156@gmail.com>
2023-07-05 01:39:06 +0300

    solid reduce princibles

--- reduce.js
+++ reduce.js
@@ -1,6 +1,13 @@
-let Arr = [1, 2];
-
-let n = Arr.reduce((e, v) => {
-    return e + v
-})
-console.log(n);
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function (nums, fn, init) {
+    let count = init;
+    nums.forEach(num => {
+        count = fn(count, num)
+    })
+    return count
+};
